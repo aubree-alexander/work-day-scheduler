2968272a7784d378bb74a189ea1f115c5db0f7e9
Aubree Alexander <aubree.alexander@gmail.com>
2022-03-30 19:29:56 -0500

    add current date/time at top of page

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var currentDay = moment().format('MMMM Do, YYYY')
+
+function dayTime () {
+    $('#currentDay').append(currentDay)
+}
+
+dayTime()
+
+
+// saveBtn.addEventListener('click', function())
+
+
