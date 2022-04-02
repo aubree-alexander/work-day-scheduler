105341a03cdb20204b021cbaebefa15136d235c5
Aubree Alexander <aubree.alexander@gmail.com>
2022-04-02 17:53:15 -0500

    add ability to save text to localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,48 @@
-var currentDay = moment().format('MMMM Do, YYYY')
 
-function dayTime () {
+//global variables
+var currentDay = moment().format('MMMM Do, YYYY')
+var currentTime = moment()
+var currentHour = currentTime.format("hA");
+var textarea = document.querySelector(".description")
+var saveBtn = document.querySelector(".saveBtn")
+
+
+//function for current date appended to top of page
+function dateDisplay () {
     $('#currentDay').append(currentDay)
 }
+dateDisplay()
 
-dayTime()
 
 
-// saveBtn.addEventListener('click', function())
 
+//save items to localstorage when save button is clicked
+$(".saveBtn").click(function() {
+    var key = textarea.value
+    console.log(key)
+    if (key) {
+        localStorage.setItem('taskText', key)
+    }
+});
+
+
+//retrieve items from local storage - IN PROGRESS
+localStorage.getItem(key)
+$(".description").innerHTML += `${key}`
+
+
+
+
+//brian's code to reference
+// $("#hour8 .description").val(localStorage.getItem("hour8"));
+// $("#hour9 .description").val(localStorage.getItem("hour9"));
+// $("#hour10 .description").val(localStorage.getItem("hour10"));
+// $("#hour11 .description").val(localStorage.getItem("hour11"));
+// $("#hour12 .description").val(localStorage.getItem("hour12"));
+
+
+
+//Step 6 - Use Jquery to retrieve data from local storage and display on page when user refreshes
+
+//Step 3 - Use dayJS to help indicate to user via timeblock color(CSS) whether time is in past, present, or future
 
