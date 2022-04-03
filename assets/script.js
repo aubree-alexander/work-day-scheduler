18a34fcf6f71a7d66004ef974c3ddb32f241af09
Aubree Alexander <aubree.alexander@gmail.com>
2022-04-03 14:07:53 -0500

    update functions to save and retrieve task content from localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,63 @@
-
 //global variables
+//date for top of page
 var currentDay = moment().format('MMMM Do, YYYY')
-var currentTime = moment()
-var currentHour = currentTime.format("hA");
-var textarea = document.querySelector(".description")
-var saveBtn = document.querySelector(".saveBtn")
+//current time to assess current hour for for loop below
+var currentTime = moment().hour()
 
 
-//function for current date appended to top of page
+//current date displayed at top of page
 function dateDisplay () {
     $('#currentDay').append(currentDay)
 }
 dateDisplay()
 
+//save tasks to localstorage
+$(".saveBtn").on("click", function () {
+    var description = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, description);
+})
+
+//display each task in appropriate time slot
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 
-//save items to localstorage when save button is clicked
-$(".saveBtn").click(function() {
-    var key = textarea.value
-    console.log(key)
-    if (key) {
-        localStorage.setItem('taskText', key)
+    // $(".time-block").each(function() {
+    
+    //     var hourParse = parseInt($(".hour").innerText)
+    //     console.log(hourParse)
+      
+    // })
+  
+
+var timeBlock = $('.time-block');
+var counter = 9;
+
+// display different color in description box depending on time
+for (var i = 0; i < timeBlock.length; i++) {
+    //select the current timeBlock based on hour, starting at 9
+    var currentBlock = $('#' + counter);
+    //get id and turn it into an integer
+    var idHour = $('.time-block')[i].id;
+    parseInt(idHour);
+    // check each id to see if it is past present or future
+    if (idHour < currentTime) {
+        currentBlock.addClass("past");
+    } else if (idHour == currentTime) {
+        currentBlock.addClass("present");
+    } else if (idHour > currentTime) {
+        currentBlock.addClass("future");
     }
-});
-
-
-//retrieve items from local storage - IN PROGRESS
-localStorage.getItem(key)
-$(".description").innerHTML += `${key}`
-
-
-
-
-//brian's code to reference
-// $("#hour8 .description").val(localStorage.getItem("hour8"));
-// $("#hour9 .description").val(localStorage.getItem("hour9"));
-// $("#hour10 .description").val(localStorage.getItem("hour10"));
-// $("#hour11 .description").val(localStorage.getItem("hour11"));
-// $("#hour12 .description").val(localStorage.getItem("hour12"));
-
-
-
-//Step 6 - Use Jquery to retrieve data from local storage and display on page when user refreshes
-
-//Step 3 - Use dayJS to help indicate to user via timeblock color(CSS) whether time is in past, present, or future
+    //increase counter to select next time block
+    counter ++;
+}
 
